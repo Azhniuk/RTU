d0fbc81d0b0fad9e2435e7157bddef841991f163
Sofia <azhniuk.s@gmail.com>
2022-10-09 18:04:06 +0300

    line-easy

---
+++ Python/line-easy.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+plt.title("Bresenham Algorithm")
+plt.xlabel("x axis")
+plt.ylabel("y axis")
+
+
+
+xBeg = 0
+yBeg = 0
+xEnd = 5
+yEnd = 5
+xCoordinates = [xBeg, xEnd]
+yCoordinates = [yBeg, yEnd]
+
+plt.plot(xCoordinates, yCoordinates)
+plt.show()

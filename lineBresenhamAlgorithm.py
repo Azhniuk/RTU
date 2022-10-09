3b3301ea0c8430413f12d43e81c47d7284778c75
Sofia <azhniuk.s@gmail.com>
2022-10-09 18:33:27 +0300

    Create lineBresenhamAlgorithm.py

---
+++ lineBresenhamAlgorithm.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+plt.title("Bresenham Algorithm")
+plt.xlabel("x axis")
+plt.ylabel("y axis")
+
+
+
+xBeg = 0
+yBeg = 0
+xEnd = 5
+yEnd = 5
+xCoordinates = [xBeg, xEnd]
+yCoordinates = [yBeg, yEnd]
+
+
+#def arrayCreation (xBeg, yBeg, xEnd, yEnd):
+
+plt.plot(xCoordinates, yCoordinates)
+plt.show()

bcb5f33f280e56af358afae0b29bd9134951947c
Sofia <azhniuk.s@gmail.com>
2022-11-27 15:30:47 +0300

    Update 14-11-2022Practical.c

--- C language/14-11-2022Practical.c
+++ C language/14-11-2022Practical.c
@@ -7,18 +7,32 @@
 int main()
 
 {
-    
-    int size, myArray[size];
+    int size, SUM = 0, max;
+   
 
     printf("What is the size of an array?\n");
     scanf("%d", &size);
+    int  myArray[size];
+    srand ( time(NULL) ); //for numbers to be random every time
     
     
-    for(int i=0; i<size; i++){
-        myArray[i] = rand()%13-6;
-        
-        printf("%5d", myArray[i]);
+    for(int i = 0; i < size; i++){
+        myArray[i] = rand() % 13 -6;
+        SUM += myArray[i];
+        printf(" %d ", myArray[i] );
     }
+    
+    printf("\nThe average is:  %d ", SUM/size);
+    
+    max = myArray[0];
+    
+    for(int i = 0; i < size; i++){
+        if (myArray[i] > max){
+            max = myArray[i];
+        }
+    }
+    
+    printf("\nMaximum is:  %d ", max);
     
     return 0;
 }

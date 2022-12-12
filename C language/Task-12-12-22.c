23e49e371abba8bfd183efe846e75e879f2e153a
Sofia <azhniuk.s@gmail.com>
2022-12-12 16:20:37 +0300

    Update Task-12-12-22.c

--- C language/Task-12-12-22.c
+++ C language/Task-12-12-22.c
@@ -1,3 +1,11 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -46,7 +54,7 @@ int max( int drow, int dcol, int arr[drow][dcol]) {
 int main()
 {
     const int drow = 3, dcol = 3;
-    int array [drow][dcol], a, b;
+    int array [drow][dcol], a, b, c;
     
     
     for (int i = 0; i < drow; i++){
@@ -59,12 +67,15 @@ int main()
     output(drow, dcol, array);
     a = min(drow, dcol, array);
     b = max(drow, dcol, array);
+    c = abs(a - b) * (-1);
     
     
     
     
-    printf("Module of minimum: %d \n", abs(a));
-    printf("Square root of maximum: %f \n", sqrt((double)b));
+    printf("Module of minimum a : %d \n", abs(a));
+    printf("Square root of maximum b : %f \n", sqrt((double)b));
+    printf("Third formula c : %d \n", c);
+    
     
    
     return 0;

50cad3b65a065fc0e2237ac2ef323e76b044ff0e
Sofia <azhniuk.s@gmail.com>
2022-12-19 11:26:01 +0300

    Update Lab2-19-12-22.c

--- C language/Lab2-19-12-22.c
+++ C language/Lab2-19-12-22.c
@@ -1,10 +1,4 @@
-/******************************************************************************
 
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 
 
 #include <stdio.h>
@@ -16,7 +10,7 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 int main()
 {
-    for (int i =1; i>0;){
+    for (int i =1; i==1;){
     int x, a, b, y;
 
     printf("Enter an integer: ");
@@ -44,7 +38,7 @@ int main()
         }
         
     }
-    printf("1 0\n");
+    printf("To exit: press \"0\", to continue: press\"1\" \n");
     scanf("%d", &i);
     }
     

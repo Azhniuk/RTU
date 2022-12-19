055ed423409f7ae6c71d1224fd24facbc6d29c93
Sofia <azhniuk.s@gmail.com>
2022-12-19 11:44:35 +0300

    Update Lab2-19-12-22.c

--- C language/Lab2-19-12-22.c
+++ C language/Lab2-19-12-22.c
@@ -1,45 +1,49 @@
+/******************************************************************************
 
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
 
 
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-#include <errno.h>
-
 
 
 int main()
 {
     for (int i =1; i==1;){
-    int x, a, b, y;
-
-    printf("Enter an integer: ");
-    scanf("%d", &x);  
-    
-    a = x * x * x + abs(x);
-    b = 7*x + 5;
-    printf("Number a = %d\n", a);
-    printf("Number b = %d\n", b);
-    
-    if (a <= b){
-        //𝑎(2𝑎 − 𝑏**2)
-        y = a * (2*a - b*b);
-        printf("y = %d\n", y);
         
-    }
-    else{
-        //2𝑎 − (2/b)
-        if (b == 0) {
-            printf("Error calculating");
+        float x, a, b, y;
+    
+        printf("Enter a Number: ");
+        scanf("%f", &x);  
+        
+        a = x * x * x + abs(x);
+        b = 7*x + 5;
+        printf("Number a = %f\n", a);
+        printf("Number b = %f\n", b);
+        
+        if (a <= b){
+            //𝑎(2𝑎 − 𝑏**2)
+            y = a * (2*a - b*b);
+            
         }
         else{
-            y = 2*a - 2/b;
-            printf("y = %d\n", y);
+            //2𝑎 − (2/b)
+            if (b == 0) {
+                printf("Error calculating");
+            }
+            else{
+                y = 2*a - 2/b;
+            }
         }
         
-    }
-    printf("To exit: press \"0\", to continue: press\"1\" \n");
-    scanf("%d", &i);
-    }
-    
+        printf("y = %f\n", y);
+        printf("To exit: press \"0\", to continue: press \"1\" \n");
+        scanf("%d", &i);
+        }
+    return 0;
 }

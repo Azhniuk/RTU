5d62fafcfbd0374cf4eca2fa3d65691e68e20a9e
Sofia <azhniuk.s@gmail.com>
2023-02-24 23:05:32 +0300

    task 4 Data structures

--- Java/Main.java
+++ Java/Main.java
@@ -26,3 +26,13 @@ public class Main {
     }
 
 }
+
+
+
+
+
+
+
+
+
+
--- Java/QueueExample.java
+++ Java/QueueExample.java
@@ -4,56 +4,54 @@ import java.util.Scanner;
 
 
 public class QueueExample {
-    public static void add(Queue<String> queue) {
-        Scanner scan = new Scanner(System.in);
-
+    public static void add(String name, Queue<String> queue, Scanner scan) {
         System.out.println("Your name: ");
-        String name = scan.nextLine();
-        queue.add(name);
-        scan.close();
+        name = scan.next();
+        queue.add(name); 
+        return;
+    }
 
+    public static void delete(Queue<String> queue, Scanner scan) {
+        queue.remove(); 
         return;
     }
 
 
+    
+
     public static void main(String[] args) {
         Queue<String> queue = new LinkedList<>();
+
+        boolean iscontinue = true;
+        Integer result;
+        String name = "L";
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Do you want to be in queue(press 1) or remove somebody from the queue(press 2)?");
-        Integer result = scan.nextInt();
-        if(result == 1){
-            add(queue);
-            System.out.println("The queue is: "+ queue);
-            
-           
-            //queue.add(name);
-            
+
+        while (iscontinue){
+        System.out.println("Do you want to be in queue(press 1) or remove somebody from the queue(press 2)?\nTo end press 0");
+        result = scan.nextInt();
+        if(result==1){
+            add(name, queue, scan);
         }
-        else if(result == 2){
+        else if(result==2 ){
+            delete(queue, scan);
+        }
+        else if(result == 0){
+            System.out.println("Good luck");
+            iscontinue = false;
         }
 
-       
-        /*
-        queue.add(2);
-        queue.add(3);
-        queue.add(4);
-        queue.add(5);
-
-        // Dequeue elements from the queue
-        int firstElement = queue.remove();
-        System.out.println("Removed element: " + firstElement);
-
-        // Print the remaining elements in the queue
-        
-
-        // Get the size of the queue
-        System.out.println("Queue size: " + queue.size());
-
-        // Check if the queue is empty
-        System.out.println("Is queue empty? " + queue.isEmpty());
-        */
-        scan.close();
+        System.out.println("The queue is: "+ queue);  
+        result = 5;   
+        }
+        scan.close();       
 
     }
+
 }
+
+        
+
+       
+    

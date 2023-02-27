172f8d80eb1c025897872c3b3818483b148b5857
Sofia <azhniuk.s@gmail.com>
2023-02-27 23:57:30 +0300

    Update Ex2.java

--- Java/27-02-23/Ex2.java
+++ Java/27-02-23/Ex2.java
@@ -1,7 +1,36 @@
 import java.util.Scanner; 
 
 public class Ex2 {
+
+    public static void main(String[] args){
+
     Scanner scan = new Scanner(System.in);
     String line = scan.nextLine();
-    count(line);    
+    count(line);
+    spaces(line); 
+    scan.close();
+    }
+
+    public static void count(String line){
+        System.out.println(line.length());
+    return;
+    }
+
+    public static void spaces(String line){
+        int spaceCount = 0;
+        for (char c : line.toCharArray()) {
+            if (c == ' ') {
+                spaceCount++;
+            }
+        }
+
+        System.out.println(spaceCount);
+   // assertThat(spaceCount).isEqualTo(EXPECTED_COUNT);
+    }
+    
+
+      
 }
+
+
+

4ea1b54831269c30d9ec5e45cbc05265cf05da09
Sofia <azhniuk.s@gmail.com>
2023-05-11 19:32:18 +0300

    4/6 Java

--- Java/HW/HW.java
+++ Java/HW/HW.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 public class HW implements ActionListener 
 {  
-    Color myWhite = new Color(205, 255, 255);
+    Color myWhite = new Color(220, 255, 255);
     JFrame frame = new JFrame("24-04");  
     JTextField t1,t2, t3;  
     JLabel label = new JLabel();
@@ -14,33 +14,25 @@ public class HW implements ActionListener
 
     private ButtonGroup buttonGroup;
      
-
     public HW() {
-        JRadioButton add = new JRadioButton("Add");
-        JRadioButton subtract = new JRadioButton("Subtract");
-        JRadioButton multiply = new JRadioButton("Multiply");
-
-        JButton calculate =new JButton("Calculate");  
-        buttonGroup = new ButtonGroup();
-
-        add.setActionCommand("+");
-        subtract.setActionCommand("-");
-        multiply.setActionCommand("*");
-
-      
+//FRAME     
         frame.setSize(300, 300);
         frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        frame.addWindowListener(new CustomWindowAdapter());
         frame.setLocationRelativeTo(null);
         frame.setLayout(null);   
+        frame.getContentPane().setBackground(Color.WHITE);
     
+//NUMBERS
         label.setText("Number 1");
-        label.setBounds(15,10,70,25);
         label1.setText("Number 2");
-        label1.setBounds(15,40,70,25);
         label2.setText("Number 3");
+        result_Label.setText("Result: no result");
+
+        label.setBounds(15,10,70,25);
+        label1.setBounds(15,40,70,25);
         label2.setBounds(15,70,70,25);
-        result_Label.setText("Result:");
-        result_Label.setBounds(25,150,200,40);
+        result_Label.setBounds(25,140,200,40);
 
 
         label.setFont(new Font("Courier", Font.PLAIN, 15)); 
@@ -48,24 +40,46 @@ public class HW implements ActionListener
         label2.setFont(new Font("Courier", Font.PLAIN, 15)); 
         result_Label.setFont(new Font("Courier", Font.PLAIN, 18)); 
      
-        //ввід тексту
+//INPUT
         t1=new JTextField("");  
-        t1.setBounds(100, 12, 150,20);  
         t2=new JTextField("");  
-        t2.setBounds(100,45, 150,20); 
         t3=new JTextField("");  
+
+        t1.setBounds(100, 12, 150,20);  
+        t2.setBounds(100,45, 150,20); 
         t3.setBounds(100,75, 150,20); 
+
+//ACTION
+        JRadioButton add = new JRadioButton("Add");
+        JRadioButton subtract = new JRadioButton("Subtract");
+        JRadioButton multiply = new JRadioButton("Multiply");
         
-        add.setBounds(10, 110, 58, 20);
-        subtract.setBounds(90, 110, 90, 20);
-        multiply.setBounds(190, 110, 70, 20);
+        add.setBounds(15, 110, 50, 20);
+        subtract.setBounds(80, 110, 85, 20);
+        multiply.setBounds(180, 110, 90, 20);
+
+        add.setFont(new Font("Courier", Font.PLAIN, 12)); 
+        subtract.setFont(new Font("Courier", Font.PLAIN, 12)); 
+        multiply.setFont(new Font("Courier", Font.PLAIN, 12)); 
+
+        add.setBackground(myWhite);
+        subtract.setBackground(myWhite);
+        multiply.setBackground(myWhite);
+
+//CALCULATE
+        JButton calculate =new JButton("Calculate");  
        
         calculate.setBounds(15, 190, 250, 40);
         calculate.setBackground(myWhite);
         calculate.setFont(new Font("TimesRoman", Font.PLAIN, 20)); 
         calculate.addActionListener(this);
-    
 
+//ADD
+        buttonGroup = new ButtonGroup();
+        buttonGroup.add(add);
+        buttonGroup.add(multiply);
+        buttonGroup.add(subtract);
+    
         frame.add(label);
         frame.add(label1);
         frame.add(label2);
@@ -78,31 +92,20 @@ public class HW implements ActionListener
         frame.add(multiply); 
         frame.add(calculate);   
 
- 
-        //add radio buttons
-        buttonGroup.add(add);
-        buttonGroup.add(multiply);
-        buttonGroup.add(subtract);
-    
         frame.setVisible(true);
-
     }
 
 
 
    
     @Override
-
     public void actionPerformed(ActionEvent e) {
-
         if (t1.getText().isEmpty() || t2.getText().isEmpty() || t3.getText().isEmpty()) {
             JOptionPane.showMessageDialog(null, "Enter three numbers", "Error", JOptionPane.ERROR_MESSAGE);
         }
         else if (notInteger(t1) || notInteger(t2)||notInteger(t3)) {
             System.out.println("Values MUST be INTEGERS");
-
-        }
-                
+        }      
         else {
             int num1 = Integer.parseInt(t1.getText());
             int num2 = Integer.parseInt(t2.getText());
@@ -127,10 +130,20 @@ public class HW implements ActionListener
 
            
         }
-
         }
       
 
+
+    class CustomWindowAdapter extends WindowAdapter {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                int option = JOptionPane.showConfirmDialog(frame, "Are you sure you want to close the program?", "Confirmation", JOptionPane.YES_NO_OPTION);
+                if (option == JOptionPane.YES_OPTION) {
+                    frame.dispose();
+                }
+            }
+        }
+    
     public static boolean notInteger(JTextField textField) {
             try {
                 Integer.parseInt(textField.getText());
@@ -144,5 +157,6 @@ public class HW implements ActionListener
 
     public static void main(String args[]){
         HW test = new HW();
+        
     }
 }
